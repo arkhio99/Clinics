41a42f7b5f2c51d8505cf6a052293d30bfaa691a
Vlad <arkhio99@users.noreply.github.com>
2020-12-23 00:01:46 +0500

    внести изм

--- Clinics/Authorization.Designer.cs
+++ Clinics/Authorization.Designer.cs
@@ -35,12 +35,14 @@
             this.RegistrationBtn = new System.Windows.Forms.Button();
             this.LoginTB = new System.Windows.Forms.TextBox();
             this.PasswordTB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(81, 139);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(44, 15);
             this.label1.TabIndex = 0;
@@ -49,7 +51,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Location = new System.Drawing.Point(80, 170);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(52, 15);
             this.label2.TabIndex = 1;
@@ -58,14 +60,14 @@
             // PasswordErrorLbl
             // 
             this.PasswordErrorLbl.AutoSize = true;
-            this.PasswordErrorLbl.Location = new System.Drawing.Point(12, 67);
+            this.PasswordErrorLbl.Location = new System.Drawing.Point(80, 193);
             this.PasswordErrorLbl.Name = "PasswordErrorLbl";
             this.PasswordErrorLbl.Size = new System.Drawing.Size(0, 15);
             this.PasswordErrorLbl.TabIndex = 2;
             // 
             // LogOnBtn
             // 
-            this.LogOnBtn.Location = new System.Drawing.Point(13, 88);
+            this.LogOnBtn.Location = new System.Drawing.Point(81, 214);
             this.LogOnBtn.Name = "LogOnBtn";
             this.LogOnBtn.Size = new System.Drawing.Size(88, 23);
             this.LogOnBtn.TabIndex = 3;
@@ -75,7 +77,7 @@
             // 
             // RegistrationBtn
             // 
-            this.RegistrationBtn.Location = new System.Drawing.Point(107, 88);
+            this.RegistrationBtn.Location = new System.Drawing.Point(175, 214);
             this.RegistrationBtn.Name = "RegistrationBtn";
             this.RegistrationBtn.Size = new System.Drawing.Size(93, 23);
             this.RegistrationBtn.TabIndex = 4;
@@ -85,23 +87,43 @@
             // 
             // LoginTB
             // 
-            this.LoginTB.Location = new System.Drawing.Point(79, 10);
+            this.LoginTB.Location = new System.Drawing.Point(147, 136);
             this.LoginTB.Name = "LoginTB";
             this.LoginTB.Size = new System.Drawing.Size(121, 23);
             this.LoginTB.TabIndex = 5;
             // 
             // PasswordTB
             // 
-            this.PasswordTB.Location = new System.Drawing.Point(79, 41);
+            this.PasswordTB.Location = new System.Drawing.Point(147, 167);
             this.PasswordTB.Name = "PasswordTB";
             this.PasswordTB.Size = new System.Drawing.Size(121, 23);
             this.PasswordTB.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(50, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(274, 61);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Добро пожаловать в сервис по оформлению визита в сеть клиник \"БС\"";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(70, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(237, 54);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Перед тем как оформить визит, войдите в свой аккаунт или зарегистрируйте новый";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Authorization
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(219, 122);
+            this.ClientSize = new System.Drawing.Size(360, 253);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.PasswordTB);
             this.Controls.Add(this.LoginTB);
             this.Controls.Add(this.RegistrationBtn);
@@ -126,5 +148,7 @@
         private System.Windows.Forms.Button RegistrationBtn;
         private System.Windows.Forms.TextBox LoginTB;
         private System.Windows.Forms.TextBox PasswordTB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
